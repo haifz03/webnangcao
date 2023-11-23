c26698a3f9c90ee1e6e3bc2a1d60f72ad5eaffbc
Haifz03 <haifz622003@gmail.com>
2023-11-23 17:27:31 +0700

    shoppingonlinegit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.tiavcqj.mongodb.net',
+    DB_USER: 'haifz622003',
+    DB_PASS: 'Phihai213',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'Haifz622003',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'haifz03', // gmail service
+    EMAIL_PASS: 'Haifz.03'
+  };
+  module.exports = MyConstants;
